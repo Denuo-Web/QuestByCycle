f761908215d525b31ec414e9e627e3f0599cadc0
Jaron Rosenau <jaron.rosenau@gmail.com>
2026-04-21 22:47:42 -0700

    Patch remaining dependency advisories and workflow drift

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,5 +1,5 @@
 // The version of the cache
-const VERSION = '1.2.1-57f6a0d'; // Update this version number when changes are made
+const VERSION = '1.2.1-23b9244'; // Update this version number when changes are made
 const CACHE_NAME = `questbycycle-${VERSION}`;
 
 // List of static resources to cache
